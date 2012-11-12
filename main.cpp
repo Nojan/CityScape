7019db7ad4e3fe3c6f151b732629ecc8394abb06
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2012-11-12 22:37:36 +0100

    Load et affichage texture

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <GL/glfw.h>
 
 #include "shader.hpp"
+#include "texture.hpp"
 
 using namespace std;
 
@@ -45,6 +46,7 @@ int main()
    }
 
    // Open an OpenGL window
+   glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 2);
    if( !glfwOpenWindow( glo_windowWidth, glo_windowHeight, 0,0,0, 0,0,0, GLFW_WINDOW ) )
    {
@@ -118,44 +120,44 @@ int main()
         1.0f,-1.0f, 1.0f
    };
 
-   // One color for each vertex. They were generated randomly.
-   static const GLfloat g_color_buffer_data[] = {
-       0.583f,  0.771f,  0.014f,
-       0.609f,  0.115f,  0.436f,
-       0.327f,  0.483f,  0.844f,
-       0.822f,  0.569f,  0.201f,
-       0.435f,  0.602f,  0.223f,
-       0.310f,  0.747f,  0.185f,
-       0.597f,  0.770f,  0.761f,
-       0.559f,  0.436f,  0.730f,
-       0.359f,  0.583f,  0.152f,
-       0.483f,  0.596f,  0.789f,
-       0.559f,  0.861f,  0.639f,
-       0.195f,  0.548f,  0.859f,
-       0.014f,  0.184f,  0.576f,
-       0.771f,  0.328f,  0.970f,
-       0.406f,  0.615f,  0.116f,
-       0.676f,  0.977f,  0.133f,
-       0.971f,  0.572f,  0.833f,
-       0.140f,  0.616f,  0.489f,
-       0.997f,  0.513f,  0.064f,
-       0.945f,  0.719f,  0.592f,
-       0.543f,  0.021f,  0.978f,
-       0.279f,  0.317f,  0.505f,
-       0.167f,  0.620f,  0.077f,
-       0.347f,  0.857f,  0.137f,
-       0.055f,  0.953f,  0.042f,
-       0.714f,  0.505f,  0.345f,
-       0.783f,  0.290f,  0.734f,
-       0.722f,  0.645f,  0.174f,
-       0.302f,  0.455f,  0.848f,
-       0.225f,  0.587f,  0.040f,
-       0.517f,  0.713f,  0.338f,
-       0.053f,  0.959f,  0.120f,
-       0.393f,  0.621f,  0.362f,
-       0.673f,  0.211f,  0.457f,
-       0.820f,  0.883f,  0.371f,
-       0.982f,  0.099f,  0.879f
+   // Two UV coordinatesfor each vertex. They were created withe Blender.
+   static const GLfloat g_uv_buffer_data[] = {
+       0.000059f, 1.0f-0.000004f,
+       0.000103f, 1.0f-0.336048f,
+       0.335973f, 1.0f-0.335903f,
+       1.000023f, 1.0f-0.000013f,
+       0.667979f, 1.0f-0.335851f,
+       0.999958f, 1.0f-0.336064f,
+       0.667979f, 1.0f-0.335851f,
+       0.336024f, 1.0f-0.671877f,
+       0.667969f, 1.0f-0.671889f,
+       1.000023f, 1.0f-0.000013f,
+       0.668104f, 1.0f-0.000013f,
+       0.667979f, 1.0f-0.335851f,
+       0.000059f, 1.0f-0.000004f,
+       0.335973f, 1.0f-0.335903f,
+       0.336098f, 1.0f-0.000071f,
+       0.667979f, 1.0f-0.335851f,
+       0.335973f, 1.0f-0.335903f,
+       0.336024f, 1.0f-0.671877f,
+       1.000004f, 1.0f-0.671847f,
+       0.999958f, 1.0f-0.336064f,
+       0.667979f, 1.0f-0.335851f,
+       0.668104f, 1.0f-0.000013f,
+       0.335973f, 1.0f-0.335903f,
+       0.667979f, 1.0f-0.335851f,
+       0.335973f, 1.0f-0.335903f,
+       0.668104f, 1.0f-0.000013f,
+       0.336098f, 1.0f-0.000071f,
+       0.000103f, 1.0f-0.336048f,
+       0.000004f, 1.0f-0.671870f,
+       0.336024f, 1.0f-0.671877f,
+       0.000103f, 1.0f-0.336048f,
+       0.336024f, 1.0f-0.671877f,
+       0.335973f, 1.0f-0.335903f,
+       0.667969f, 1.0f-0.671889f,
+       1.000004f, 1.0f-0.671847f,
+       0.667979f, 1.0f-0.335851f
    };
 
    GLuint vertexbuffer;
@@ -163,10 +165,10 @@ int main()
    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-   GLuint colorbuffer;
-   glGenBuffers(1, &colorbuffer);
-   glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-   glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+   GLuint uvbuffer;
+   glGenBuffers(1, &uvbuffer);
+   glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+   glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
 
    // Create and compile our GLSL program from the shaders
    GLuint programID = LoadShaders( "../shader/SimpleVertexShader.vertexshader", "../shader/SimpleFragmentShader.fragmentshader" );
@@ -174,7 +176,12 @@ int main()
 
    // Get a handle for our buffers
    GLuint vertexPosition_modelspaceID = glGetAttribLocation(programID, "vertexPosition_modelspace");
-   GLuint vertexColorID = glGetAttribLocation(programID, "vertexColor");
+   GLuint vertexUVID = glGetAttribLocation(programID, "vertexUV");
+
+   GLuint Texture = loadBMP_custom("../asset/uvtemplate.bmp");
+
+   // Get a handle for our "myTextureSampler" uniform
+   GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
    // Get a handle for our "MVP" uniform
    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
@@ -216,6 +223,12 @@ int main()
            // in the "MVP" uniform
            glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
+           // Bind our texture in Texture Unit 0
+           glActiveTexture(GL_TEXTURE0);
+           glBindTexture(GL_TEXTURE_2D, Texture);
+           // Set our "myTextureSampler" sampler to user Texture Unit 0
+           glUniform1i(TextureID, 0);
+
            // 1rst attribute buffer : vertices
            glEnableVertexAttribArray(0);
            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -228,16 +241,16 @@ int main()
                (void*)0                     // array buffer offset
            );
 
-           // 2nd attribute buffer : colors
+           // 2nd attribute buffer : UVs
            glEnableVertexAttribArray(1);
-           glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+           glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
            glVertexAttribPointer(
-               vertexColorID,               // The attribute we want to configure
-               3,                           // size
-               GL_FLOAT,                    // type
-               GL_FALSE,                    // normalized?
-               0,                           // stride
-               (void*)0                     // array buffer offset
+               vertexUVID,                   // The attribute we want to configure
+               2,                            // size : U+V => 2
+               GL_FLOAT,                     // type
+               GL_FALSE,                     // normalized?
+               0,                            // stride
+               (void*)0                      // array buffer offset
            );
 
            // Draw the triangleS !
@@ -255,6 +268,7 @@ int main()
 
    // Cleanup VBO
    glDeleteBuffers(1, &vertexbuffer);
+   glDeleteBuffers(1, &uvbuffer);
 
    // Exit program
    exit( EXIT_SUCCESS );
